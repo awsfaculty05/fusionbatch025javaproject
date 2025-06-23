1ba327c1befdc2603fc30a2345836c56da9eb498
awsfaculty05 <awsfaculty05@fusion-institute.com>
2025-06-23 21:25:24 +0530

    first

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Cloud World. Thanks!!! .... New ModifiedVersion has came now.....new entry in table');
+  res.send('Hello DevOps Cloud World. Thanks!!! .... New ModifiedVersion has came now.....new entry in table final Good newsssss');
 });
 
 app.listen(PORT, HOST);
